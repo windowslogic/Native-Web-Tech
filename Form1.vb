9fa1a7cf400884f43994088e70913800e710f78c
Alethila <39615739+windowslogic@users.noreply.github.com>
2023-08-04 18:08:44 +0100

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WebBrowser1.Navigate(Application.StartupPath.ToString & "/webview/index.html")
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If WebBrowser1.DocumentTitle.Contains("close") Then
+            Me.Close()
+        End If
+    End Sub
+End Class
